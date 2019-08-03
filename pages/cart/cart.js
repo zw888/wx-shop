e6322e54fed2702cbeb8dd4378e051f615e63651
18755489161@163.com <18755489161@163.com>
2019-08-03 22:07:56 +0800

    111

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,46 +1,51 @@
 // pages/cart/cart.js
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
-
+    isSelect: false,
+    fruit: [{
+      id: 1,
+      name: '香蕉',
+    }, {
+      id: 2,
+      name: '苹果'
+    }, {
+      id: 3,
+      name: '西瓜'
+    }, {
+      id: 4,
+      name: '葡萄',
+    }],
+    current: '苹果',
+    position: 'left',
+    animal: '熊猫',
+    checked: false,
+    disabled: false,
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  handleFruitChange({ detail = {} }) {
+    this.setData({
+      current: detail.value
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleClick() {
+    this.setData({
+      position: this.data.position.indexOf('left') !== -1 ? 'right' : 'left',
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
+  handleDisabled() {
+    this.setData({
+      disabled: !this.data.disabled
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
+  handleAnimalChange({ detail = {} }) {
+    this.setData({
+      checked: detail.current
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
+  switchIsSelectd(){
+    console.log(1111111111)
+    this.setData({
+      isSelect: !this.data.isSelect
+    })
   },
 
   /**
